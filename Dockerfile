a490d9f4fdd68530e828892aec8ce573032cd273
Collin Drake <collinlindendrake@gmail.com>
2023-01-21 11:44:12 -0700

    JSON

--- Dockerfile
+++ Dockerfile
@@ -9,3 +9,25 @@ ADD main.py /
 RUN pip install webhooks
 
 CMD [ "python", "./main.py" ]
+
+# Use an official Python runtime as the base image
+FROM python:3
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the requirements file into the container
+COPY requirements.txt .
+
+# Install the necessary dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the application code into the container
+COPY . .
+
+# Expose the port for the websocket listener
+EXPOSE 8000
+
+# Run the websocket listener script
+CMD ["python", "websocket_listener.py"]
+
--- VM/main.py
+++ VM/main.py
@@ -1,12 +1,74 @@
+# Native:
 import os
 import json
+import asyncio
+import websockets
+# External:
 import openai
+import smtplib
+import imaplib
 
 openai.organization = "org-qcHPiKIimtg6ssjx0Xla5AGH"
 openai.api_key = "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"
 openai.Model.retrieve("text-davinci-003")
 # openai.Model.list()
 
+# Connect to the email server
+mail = imaplib.IMAP4_SSL("imap.gmail.com")
+mail.login("your_email@gmail.com", "your_password")
+
+# Select the inbox
+mail.select("inbox")
+
+# Search for unread messages
+status, messages = mail.search(None, "UNSEEN")
+
+# Get the first unread message
+message_id = messages[0].split()[0]
+
+# Fetch the message
+status, message = mail.fetch(message_id, "(RFC822)")
+
+# Parse the message
+message = message[0][1]
+
+# Convert the message to JSON
+message_json = json.dumps(message.decode())
+
+async def send_to_websocket(websocket, path):
+    await websocket.send(message_json)
+
+# Connect to the websocket server
+websocket = websockets.serve(send_to_websocket, "localhost", 8000)
+
+asyncio.get_event_loop().run_until_complete(websocket)
+asyncio.get_event_loop().run_forever()
+
 response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
 
-print(json.dumps(response["choices"]))
+# Load the JSON file
+with open("response.json", "r") as json_file:
+    data = json.load(json_file)
+
+# Extract the email information from the JSON file
+name = data["name"]
+email = data["email address"]
+timestamp = data["timestamp"]
+text = data["text"]
+
+# Create the email message
+message = f"Dear {name},\n\n{text}\n\nSincerely,\nYour Name\nTimestamp:{timestamp}"
+
+# Connect to the email server
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+
+# Log in to the email account
+server.login("your_email@gmail.com", "your_password")
+
+# Send the email
+server.sendmail("your_email@gmail.com", email, message)
+
+# Close the connection to the email server
+server.quit()
+
