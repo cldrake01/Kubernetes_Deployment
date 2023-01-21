6cdc9e5693f2a9b1be5e62161aa0e6ee507e3af2
Collin Drake <collinlindendrake@gmail.com>
2023-01-20 23:46:22 -0700

    JSON

--- VM/main.py
+++ VM/main.py
@@ -5,7 +5,7 @@ import openai
 openai.organization = "org-qcHPiKIimtg6ssjx0Xla5AGH"
 openai.api_key = "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"
 openai.Model.retrieve("text-davinci-003")
-openai.Model.list()
+# openai.Model.list()
 
 response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
 
